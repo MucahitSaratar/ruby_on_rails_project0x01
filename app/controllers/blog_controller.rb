c1539ac833cd1fef0ee8d3589babd6440514f45f
mucahitsaratar <trregen222@gmail.com>
2021-03-07 21:09:55 +0300

    listeleme isi kismen cozuldu

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,11 +1,6 @@
 class BlogController < ApplicationController
   def index
       db = SQLite3::Database.open "db/development.sqlite3"
-      stm = db.query "select * from yazilarim"
-      cek = stm.next
-      @id = cek[0]
-      @baslik = cek[1]
-      @logo = cek[2]
-      @icerik = cek[3]
+      @stm = db.query "select * from yazilarim"
   end
 end
