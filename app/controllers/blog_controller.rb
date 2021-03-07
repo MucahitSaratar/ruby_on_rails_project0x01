ea8bfa744a3ed35b7c6013c52976ce2b97ecd645
mucahitsaratar <trregen222@gmail.com>
2021-03-08 01:50:40 +0300

    biseyler biseyler

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,9 +1,10 @@
 class BlogController < ApplicationController
     def index
-      db = SQLite3::Database.open "db/development.sqlite3"
-      id = params[:id].to_i
-      @stm = db.query "select * from yazilarim where id > ? and id < ?",id*10,(id+1)*10
+        db = SQLite3::Database.open "db/development.sqlite3"
+        id = params[:id].to_i
+        @stm = db.query "select * from yazilarim where id > ? and id < ?",id*10,(id+1)*10
   end
   def read
+      id = params[:id].to_i
   end
 end
