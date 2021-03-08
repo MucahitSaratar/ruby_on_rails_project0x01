10a4b6fae26cc993e7a428fe715d868ce823a855
mucahitsaratar <trregen222@gmail.com>
2021-03-08 20:38:02 +0300

    read renderine basladim

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -5,6 +5,18 @@ class BlogController < ApplicationController
         @stm = db.query "select * from yazilarim where id > ? and id < ?",@id*10,(@id+1)*10
   end
   def read
-      id = params[:id].to_i
+      db = SQLite3::Database.open "db/development.sqlite3"
+      @id = params[:id].to_i
+      stm = db.query "select * from detayliyazi where id=?",@id
+      @row = stm.next
+      p @row
+      if @row == nil
+        @basligim = "<h1>Hatalı istek</h1>"
+      else
+        @basligim = @row[1]
+        @icerik = @row[2]
+        @icerik["---"] = "<div class='code'>"
+        @icerik["--"] = "</div>"
+      end
   end
 end
